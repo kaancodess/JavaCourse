58d07875d9a4517b97944a175931c1c39113cfc5
kaancodes <112093775+kaancodess@users.noreply.github.com>
2023-03-20 21:11:27 +0200

    Add files via upload

--- JavaCourse/LocalVariablesInCodeBlocks/main.java
+++ JavaCourse/LocalVariablesInCodeBlocks/main.java
@@ -1,12 +1,10 @@
 public class Main{
+	public static void sum(int a , int b , int c){
+		
+		System.out.println("sum of numbers is " + (a+b+c ));
+	}
 	public static void main(String args[]){
-		/*int a = 4;
-		String print = "Hello";
-
-		if(a < 10){
-			int b = 5;
-			System.out.println(print);*/
-		}
-		// System.out.println(b); // we can't print b because b is undefined, b is defined in if section
+		sum(3,5,8);
+		System.out.println(a); // we can't see the a because we define in our method sum
 	}
 }
