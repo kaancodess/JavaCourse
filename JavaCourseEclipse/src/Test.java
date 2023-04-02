12c2f38800be0799ab983a4c314ec63adc0fdbf4
kaancodes <112093775+kaancodess@users.noreply.github.com>
2023-04-02 03:40:00 +0300

    Add files via upload

---
+++ JavaCourseEclipse/src/Test.java
@@ -0,0 +1,17 @@
+
+public class Test {
+
+	public static void main(String[] args) {
+		car car1 = new car();
+
+		car1.color = "White";
+		car1.wheels = 4;
+		car1.doors = 4;
+		car1.engine = "1.4";
+		car1.brand = "Toyota";
+
+		System.out.println(car1.engine);
+
+	}
+
+}
---
+++ JavaCourseEclipse/src/car.java
@@ -0,0 +1,9 @@
+
+public class car {
+	
+	public String color;
+	public int doors;
+	public int wheels;
+	public String engine;
+	public String brand;
+}
